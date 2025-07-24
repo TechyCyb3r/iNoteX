f3697520192efb546edf871ed6e01eebb5d8eedc
Himanshu Agarwal <134074266+TechyCyb3r@users.noreply.github.com>
2025-07-24 12:23:20 +0530

    Notes Card replacement

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,15 +1,20 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import Icon from '@mdi/react';
 import { mdiTrashCan, mdiPencil } from '@mdi/js';
 import styles from '../Css/NoteItem.module.css';
 import noteContext from './Context/notes/NoteContext';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 const NoteItem = (props) => {
 
     const context = useContext(noteContext);
     const { deleteNote } = context;
     const { note, updateNote, showAlert } = props;
-
 
     const handleDeleteNote = () => {
         const confirm = window.confirm("Are you sure you want to delete this note?");
@@ -21,29 +26,34 @@ const NoteItem = (props) => {
         updateNote(note);
         showAlert("Note Update successfully", "primary");
     }
-    return (
-        <div className=" col-md-3" style={{ width: "25rem" }}>
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">{note.title}</h5>
-                    <p className="card-text">{note.description}</p>
-                </div>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span>{new Date(note.date).toLocaleDateString()}</span>
+    const card = (
+        <React.Fragment>
+            <CardContent>
+                <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
                         <span>{new Date(note.date).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</span>
-                    </li>
-                </ul>
+                        <span>{new Date(note.date).toLocaleDateString()}</span>
+                    </Box>
+                </Typography>
+                <Typography variant="h5" component="div">
+                    {note.title}
+                </Typography>
+                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}><span className="badge text-bg-success">{note.tag}</span></Typography>
+                <Typography variant="body2">
+                    {note.description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Icon className={`${styles.icon} mx-1`} path={mdiTrashCan} size={1} onClick={handleDeleteNote} />
+                <Icon className={`${styles.icon} mx-2`} note={note} path={mdiPencil} onClick={handleUpdateNote} size={1} />
+            </CardActions>
+        </React.Fragment>
+    );
 
-                {/* // Using icons for delete and edit actions */}
-                <div className="card-body">
-                    <span className="badge rounded-pill text-bg-success">{note.tag}</span>
-                    <Icon className={`${styles.icon} mx-1`} path={mdiTrashCan} size={1} onClick={handleDeleteNote} />
-                    <Icon className={`${styles.icon} mx-2`} note={note} path={mdiPencil} onClick={handleUpdateNote} size={1} />
-                </div>
-            </div>
-        </div>
+    return (
+        <Box sx={{ minWidth: 100, maxWidth: 345, margin: '1rem' }}>
+            <Card variant="outlined">{card}</Card>
+        </Box>
     )
 }
-
 export default NoteItem;
