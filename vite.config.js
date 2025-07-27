7f40e519ee54ce270109a9a6c87f43d8d4bf2640
Himanshu Agarwal <134074266+TechyCyb3r@users.noreply.github.com>
2025-07-27 09:39:08 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Required for Vercel to understand routing
   server: {
     port: 3000,
     strictPort: true
   }
 })
-
