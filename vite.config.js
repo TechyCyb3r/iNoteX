64588f8fbd099cfbca45659990cebb7f1c43cad9
Himanshu Agarwal <134074266+TechyCyb3r@users.noreply.github.com>
2025-07-27 12:12:30 +0530

    x

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
+    outDir: 'dist'
   },
   server: {
     port: 3000,
     strictPort: true
-  },
-  // This is important for correct public path in production
-  base: '/'
-});
+  }
+})
+
