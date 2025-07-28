0bc9700b15317bfc27b87be8959cdaea4f80081a
Himanshu Agarwal <134074266+TechyCyb3r@users.noreply.github.com>
2025-07-28 10:20:12 +0530

    changes

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
-import Styles from "../css/Background.module.css";
+import Styles from "../Css/Background.module.css";
 
 export default function ParticlesBackground() {
   const [init, setInit] = useState(false);
