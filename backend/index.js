02700be0500531a2fd46149936a2888bb60e6ed1
Himanshu Agarwal <himanshuagarwalskr@gmail.com>
2025-11-12 23:27:16 +0530

    changes in index.js  in backend

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const connectToMongo = require('./db');
 
-// Connect to MongoDB
 connectToMongo();
 
 const app = express();
-
-// ✅ This line is very important for parsing JSON from frontend
 app.use(express.json());
-
-// Enable CORS
 app.use(cors({
   origin: [
     "http://localhost:5173",
@@ -22,16 +17,14 @@ app.use(cors({
   credentials: true
 }));
 
-// Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
-// Test route
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-module.exports = app; // for vercel
+module.exports = app;
 
 if (require.main === module) {
   const port = process.env.PORT || 5000;
