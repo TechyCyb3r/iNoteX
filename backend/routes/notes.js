5df28aff496e18639d60a67d55c26497e77346da
Himanshu Agarwal <himanshuagarwalskr@gmail.com>
2025-11-13 01:49:07 +0530

    Update notes.js
    changes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -18,35 +18,37 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
 
 
 // Route 2: Add a new node using POST "/api/auth/addnote "
-router.post('/addnote', fetchuser, [
+router.post('/addnote', fetchuser,
+    //     , [
     // using check
     // body('title', 'Enter a valid title').isLength({ min: 3 }),
     // body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),
 
-], async (req, res) => {
+    // ],
+    async (req, res) => {
 
-    try {
-        const { title, description, tag } = req.body;
-        if (!title && !description) {
-            return res.status(400).json({ error: "Please enter a title or description." });
+        try {
+            const { title, description, tag } = req.body;
+            if (!title && !description) {
+                return res.status(400).json({ error: "Please enter a title or description." });
+            }
+
+            // If there are errors, return Bad request and the errors
+            // const errors = validationResult(req);
+            // if (!errors.isEmpty()) {
+            //     return res.status(400).json({ errors: errors.array() });
+            // }
+
+            const note = new Note({
+                title, description, tag, user: req.user.id
+            })
+            const savedNote = await note.save()
+            res.json(savedNote);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Serrver Error");
         }
-
-        // If there are errors, return Bad request and the errors
-        // const errors = validationResult(req);
-        // if (!errors.isEmpty()) {
-        //     return res.status(400).json({ errors: errors.array() });
-        // }
-
-        const note = new Note({
-            title, description, tag, user: req.user.id
-        })
-        const savedNote = await note.save()
-        res.json(savedNote);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Internal Serrver Error");
-    }
-})
+    })
 
 
 // Route 3: Update an existing Note using POST "/api/auth/updatenote"
